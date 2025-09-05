792d7211d0344af3fd30f29708a1d01540c87321
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-09-05 07:58:10 -0300

    First Update

---
+++ appAsks/src/app/010_question/question-type01/question-type01.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-type01',
+  templateUrl: './question-type01.component.html',
+  styleUrls: ['./question-type01.component.scss']
+})
+export class QuestionType01Component {
+
+}
---
+++ appAsks/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './000_core/navbar/navbar.component';
+import { HeaderComponent } from './000_core/header/header.component';
+import { FooterComponent } from './000_core/footer/footer.component';
+import { QuestionType01Component } from './010_question/question-type01/question-type01.component';
+import { ThemaComponent } from './010_question/thema/thema.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    QuestionType01Component,
+    ThemaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
